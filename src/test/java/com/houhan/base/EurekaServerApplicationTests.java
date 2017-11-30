3fdf36e96be146d9819068b9f0ccd5a4bb0a92d5
tft@houhan.com <pwoftftatHH24>
2017-11-30 18:01:32 +0800

    初次提交

---
+++ src/test/java/com/houhan/base/EurekaServerApplicationTests.java
@@ -0,0 +1,18 @@
+package com.houhan.base;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.crypto.password.StandardPasswordEncoder;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class EurekaServerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
